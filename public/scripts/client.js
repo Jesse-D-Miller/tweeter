e25022853cef861637b745f6302c4ae921f1f991
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-24 08:12:03 -0700

    implemented function loadTweets that makes an ajax GET request to an API and then renders the tweets. i also implememented timeago.format to format the posted date relative to the current time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,6 +18,7 @@ $(document).ready(() => {
     console.log("Serialized form data:", formData);
 
     //idk what ajax does and how it invokes an implicit promise
+    //note: i kind of get this now
     $.ajax({
       method: "POST",
       url: "http://localhost:8080/api/tweets",
@@ -34,36 +35,27 @@ $(document).ready(() => {
         console.error("AJAX error:", err);
       });
   });
-});
 
-//data and helper functions
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+  const loadTweets = () => {
+    //ajax/jquery get request to get tweet data from /api/tweets
+    $.ajax({
+      method: "GET",
+      url: "http://localhost:8080/api/tweets",
+      // data: user,
+    })
+      //promise with a response from the server and a form reset or an error 
+      .then((response) => {
+        console.log("Server responded:", response);
+        // reset form
+        renderTweets(response);
+      })
+      .catch((err) => {
+        console.error("AJAX error:", err);
+      });
   }
-]
+
+  loadTweets();
+});
 
 // loops through tweets
 // calls createTweetElement for each tweet
@@ -97,7 +89,7 @@ const createTweetElement = (tweet) => {
 
   // footer
   const $footer = $("<footer>");
-  const $postedDate = $("<span>").text(created_at);
+  const $postedDate = $("<span>").text(timeago.format(created_at));
   const $iconsDiv = $("<div>");
   const $flagIcon = $("<i>").addClass("fa-solid fa-flag");
   const $retweetIcon = $("<i>").addClass("fa-solid fa-retweet");
@@ -110,5 +102,3 @@ const createTweetElement = (tweet) => {
 
   return $article;
 }
-
-renderTweets(data);
